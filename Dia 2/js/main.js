312b3f9e8a94ecd9cd08bc7df656c13e162b6fe3
kenia <keniayhdiaz@gmail.com>
2024-04-12 09:00:56 -0500

    feat: :sparkles: Se agregaron nuevas funciones al main.js

--- Dia 2/js/main.js
+++ Dia 2/js/main.js
@@ -51,12 +51,51 @@ while (end == true) {
                     console.log("1. Crear nuevo Trainer \t2. Asignar horario \t3.Ver Trainers \n4. Volver al menú anterior");
                     let optionT = prompt("Digite el número según la opción a la deseas ingresar: ");
                     if (optionT == 1) {
-
+                        //aca va una impo
                     }
-                    
+                    else if (optionT == 2){
+                        //aca va otra impo
+                    }
+                    else if ( optionT == 3) {
+                        //otra impo
+                    }
+                    else {
+                        if (optionT == 4){
+                            comeBackC = false;
+                        }
+                    }
                 }
             }
-        }
+            else if (optionC == 2) {
+                let comeBackC = true
+                while (comeBackC == true) {
+                    letreroC();
+                    console.log("Has ingresado a la sección de Campers");
+                    console.log("¿Qué deseas hacer?");
+                    console.log("1. Incripciones \t2. Matriculas \n3. Editar información \t4.Notas \n5. Volver al menú anterior");
+                    let optionCa = prompt("Digite el número según la opción a la que deseas ingresar: ");
+                    if (optionCa == 1) {
+                        console.log("Favor ingresar datos de ");
+                        //impo
+                        //impo
+                        console.log("Listo!");
+                    }
+                    else if (optionCa == 2) {
+                        console.log("¿Deseas actualizar los campers que han sido aprobados?");
+                        let optionSN = prompt("Ingresa si o no: ")
+                        if (sd == 'si'){
+                            //impo
+                            
+                        }
+                    }
+                }
+
+            }
+        
+        
+        }    
     } 
+
+
 }   
 
