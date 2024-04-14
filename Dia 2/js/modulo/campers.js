2cce7bef5937646df4dde7cb25d39a68d46203fc
Kenia <keniayhdiaz@gmail.com>
2024-04-14 10:52:28 -0500

    feat: :hammer: Se crearon nuevos script

---
+++ Dia 2/js/modulo/campers.js
@@ -0,0 +1,10 @@
+//------------------------------------------------------------
+//-                  FUNCIONES CAMPERS                       -
+//------------------------------------------------------------
+
+export function addCampers(){
+    return "aun no está"
+}
+export function notasCampers(){
+    return "aun no está"
+}
---
+++ Dia 2/js/modulo/fuction.js
@@ -0,0 +1,15 @@
+// Letreros par del menú
+export function letreroC(){
+    console.log(`
+    ------------------------------
+    -       ROL COORDINACIÓN     -
+    -          ACADÉMICA         -
+    ------------------------------`);
+}
+
+export function letreroT(){
+    console.log(`
+    ------------------------------
+    -         ROL TRAINERS       -
+    ------------------------------`);
+}
