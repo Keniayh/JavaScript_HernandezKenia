6f7b7087016f1733483513ea8b3bdbecd4a9afcc
kenia <keniayhdiaz@gmail.com>
2024-04-25 14:06:20 -0500

    feat: :alien: Codigo de API

--- Dia 7/js/main.js
+++ Dia 7/js/main.js
@@ -1,5 +1,32 @@
-function fectchSuperHero() {
+function fetchSuperHero() {
     let xhr = new XMLHttpRequest();
     let heroID = document.getElementById('heroId').value;
+    console.log(heroID);
+    let apiKey = "ba61442990951c9c1a0e3005ff48295f";
+    let url = `https://superheroapi.com/api.php/${apiKey}/${heroID}`;
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status == 200){
+            let response = JSON.parse(this.responseText);
+            console.log(response);
+            displayHero(response)
+        }
+        else if(this.readyState == 4){
+            console.log('Errot:', this.statusText)
+        }
+    };
+    xhr.send();
+}
 
+function displayHero(data) {
+    let heroInfo = document.getElementById('superHeroInfo');
+    if (data.response === "error"){
+        heroInfo.innerHTML = `<p>Error: ${data.error}</p>`;
+    }
+    else {
+        heroInfo.innerHTML = `
+        <p>Name: ${data.name}</p>
+        <p>Inteligencia: ${data.powerstats.intelligence}</p>
+        `
+    }
 }
