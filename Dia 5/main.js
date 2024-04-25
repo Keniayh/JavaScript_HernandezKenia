d8ae318dd371badfa5085f7160943d4e27925f17
kenia <keniayhdiaz@gmail.com>
2024-04-25 14:04:42 -0500

    feat: :sparkles: Realización de la acyividad

--- Dia 5/main.js
+++ Dia 5/main.js
@@ -2,3 +2,86 @@
 //                           QUEENS 
 //-------------------------------------------------------------------
 
+function esSolucionValida(solucion) {
+    for (let i = 0; i < solucion.length; i++) {
+        for (let j = i + 1; j < solucion.length; j++) {
+            if (solucion[i] === solucion[j] || Math.abs(solucion[i] - solucion[j]) === Math.abs(i - j)) {
+                return false;
+
+            }
+        }
+    }
+    return true;
+}
+
+function generarSoluciones() {
+    const soluciones = [];
+    const filaActual = [];
+    buscarSoluciones(0, filaActual, soluciones);
+    return soluciones;
+}
+
+function buscarSoluciones(columna, filaActual, soluciones) {
+    if (columna === 8) {
+        if (esSolucionValida(filaActual)) {
+            soluciones.push([...filaActual]);
+        }
+        return;
+    }
+
+    for (let fila = 0; fila < 8; fila++) {
+        filaActual[columna] = fila;
+        buscarSoluciones(columna + 1, filaActual, soluciones);
+    }
+}
+
+function ordenarSolucionesConBurbuja(soluciones) {
+    const n = soluciones.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (ataquesEntreReinas(soluciones[j]) > ataquesEntreReinas(soluciones[j + 1])) {
+                // Intercambiar soluciones utilizando destructuring
+                [soluciones[j], soluciones[j + 1]] = [soluciones[j + 1], soluciones[j]];
+            }
+        }
+    }
+}
+
+function ataquesEntreReinas(solucion) {
+    let ataques = 0;
+    for (let i = 0; i < solucion.length; i++) {
+        for (let j = i + 1; j < solucion.length; j++) {
+            if (solucion[i] === solucion[j] || Math.abs(solucion[i] - solucion[j]) === Math.abs(i - j)) {
+                ataques++;
+            }
+        }
+    }
+    return ataques;
+}
+
+const soluciones = generarSoluciones();
+
+ordenarSolucionesConBurbuja(soluciones);
+
+
+function imprimirTablero(solucion) {
+    const tablero = [];
+    for (let fila = 0; fila < 8; fila++) {
+        let filaStr = '';
+        for (let columna = 0; columna < 8; columna++) {
+            if (solucion[columna] === fila) {
+                filaStr += '(Q)';
+            } else {
+                filaStr += '*';
+            }
+        }
+        tablero.push(filaStr);
+    }
+    console.log(tablero.join('\n'));
+    console.log('\n');
+}
+
+soluciones.forEach((solucion, index) => {
+    console.log(`Solución ${index + 1}:`);
+    imprimirTablero(solucion);
+});
