7129fd31885c6d9aa23d14e1415f7b6f46996ae2
Kenia <keniayhdiaz@gmail.com>
2024-04-26 00:42:56 -0500

    feat: :sparkles: Nuevas funciones

--- Dia 9/Dia 8/js/api.js
+++ Dia 9/Dia 8/js/api.js
@@ -1,53 +1,136 @@
 function fetchPerson() {
     let xhr = new XMLHttpRequest();
     let personID = document.getElementById('personId').value;
-    console.log(personID);
     let url = `https://swapi.py4e.com/api/people/${personID}`;
     xhr.open('GET', url, true);
-    xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status == 200){
-            let response= JSON.parse(this.responseText);
-            console.log(response);
-            displayPeople(response)
-            peopleInfo(response)
-        }
-        else if(this.readyState == 4){
-            console.log('Errot:', this.statusText)
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status == 200) {
+                let response = JSON.parse(this.responseText);
+                console.log(response);
+                mostrarPersona(response);
+            } else {
+                console.log('Error:', this.statusText);
+                mostrarError(this.statusText);
+            }
         }
     };
     xhr.send();
 }
 
-function displayPeople(data){
+function mostrarPersona(data) {
     let personInfo = document.getElementById("peopleInfo");
-    
-    if (data.response === "error") {
-        personInfo.innerHTML = `<p>Error: ${data.error}</p>`;
-    } else {
-        personInfo.innerHTML = `
-        <h3>Datos de la persona</h3>
-        <p>Nombre: ${data.name}</p>
-        <p>Height: ${data.height}</p>
-        <p>Mass: ${data.mass}</p>
-        <p>Hair Color: ${data.hair_color}</p>
-        <p>Skin Color: ${data.skin_color}</p>
-        <p>Birth Year: ${data.birth_year}</p>
-        <p>Gender: ${data.gender}</p>
-        <br></br>
-        <p>Homeworld: ${data.homeworld}</p>
-        <br></br>
-        <p>Films: ${data.films}</p>
-        <br></br>
-        <p>Species: ${data.species}</p>
-        <br></br>
-        <p>Vehicles: ${data.vehicles}</p>
-        <br></br>
-        <p>Starships: ${data.starships}</p>
-        <br></br>
-        <p><Created: ${data.created}/p>
-        <p>Edited: ${data.edited}</p>
-        <p>URL: ${data.url}</p>
-        `;
-    }
-    
+
+    personInfo.innerHTML = `
+    <h3>Datos de la persona</h3>
+    <div class="table-responsive">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">Campo</th>
+                <th scope="col">Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Nombre</td>
+                <td>${data.name}</td>
+            </tr>
+            <tr>
+                <td>Altura</td>
+                <td>${data.height}</td>
+            </tr>
+            <tr>
+                <td>Peso</td>
+                <td>${data.mass}</td>
+            </tr>
+            <tr>
+                <td>Color de pelo</td>
+                <td>${data.hair_color}</td>
+            </tr>
+            <tr>
+                <td>Color de piel</td>
+                <td>${data.skin_color}</td>
+            </tr>
+            <tr>
+                <td>Año de nacimiento</td>
+                <td>${data.birth_year}</td>
+            </tr>
+            <tr>
+                <td>Género</td>
+                <td>${data.gender}</td>
+            </tr>
+            <tr>
+                <td>Planeta natal</td>
+                <td><span id="homeworld"></span></td>
+            </tr>
+            <tr>
+                <td>Películas</td>
+                <td><span id="films"></span></td>
+            </tr>
+            <tr>
+                <td>Especies</td>
+                <td><span id="species"></span></td>
+            </tr>
+            <tr>
+                <td>Vehículos</td>
+                <td><span id="vehicles"></span></td>
+            </tr>
+            <tr>
+                <td>Naves estelares</td>
+                <td><span id="starships"></span></td>
+            </tr>
+            <tr>
+                <td>Creado</td>
+                <td>${data.created}</td>
+            </tr>
+            <tr>
+                <td>Editado</td>
+                <td>${data.edited}</td>
+            </tr>
+            <tr>
+                <td>URL</td>
+                <td>${data.url}</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+    `;
+
+    // Obtener detalles adicionales
+    obtenerDetalle(data.homeworld, "homeworld");
+    obtenerDetalles(data.films, "films");
+    obtenerDetalles(data.species, "species");
+    obtenerDetalles(data.vehicles, "vehicles");
+    obtenerDetalles(data.starships, "starships");
+}
+
+function obtenerDetalle(url, elementId) {
+    let elemento = document.getElementById(elementId);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            elemento.innerHTML = JSON.stringify(data);
+        })
+        .catch(error => console.error('Error al obtener detalles adicionales:', error));
+}
+
+function obtenerDetalles(urls, elementId) {
+    let elemento = document.getElementById(elementId);
+    urls.forEach(url => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (elemento.innerHTML !== "") {
+                    elemento.innerHTML += "<br>";
+                }
+                elemento.innerHTML += JSON.stringify(data);
+            })
+            .catch(error => console.error('Error al obtener detalles adicionales:', error));
+    });
+}
+
+function mostrarError(error) {
+    let personInfo = document.getElementById("peopleInfo");
+    personInfo.innerHTML = `<p>Error: ${error}</p>`;
 }
