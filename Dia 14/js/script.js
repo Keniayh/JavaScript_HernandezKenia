aa7644933bfa11ba0623024de27b37879f4ebbda
kenia <keniayhdiaz@gmail.com>
2024-05-07 09:13:02 -0500

    feat: :sparkles: Avaces en el consumo de la api

--- Dia 14/js/script.js
+++ Dia 14/js/script.js
@@ -20,7 +20,35 @@ function mostrarPersona(data) {
     let personInfo = document.getElementById("person");
 
     personInfo.innerHTML = `
-    
+    <div class="name">
+            <img src="" alt="hi" id="img">
+            </div>
+            <div class="icons">
+                <div class="tooltip">
+                    <button><img src="" alt="name"></button>
+                    <div class="tooltiptext"> Hi my name is: <br></br> ${data.results[0].name.first} ${data.results[0].name.last}</div>
+                </div>
+                <div class="tooltip">
+                    <button><img src="" alt="email"></button>
+                    <div class="tooltiptext">My email address is: <br></br> ${data.results[0].email}<br></br></div>
+                </div>
+                <div class="tooltip">
+                    <button><img src="" alt="birthday"></button>
+                    <div class="tooltiptext">You Hovered Me!</div>
+                </div>
+                <div class="tooltip">
+                    <button><img src="" alt="address"> </button>
+                    <div class="tooltiptext">My address is: <br></br> ${data.results[0].location.street.number} ${data.results[0].location.street.name}</div>
+                </div>
+                <div class="tooltip">
+                    <button><img src="" alt="number"></button>
+                    <div class="tooltiptext">My address is: <br></br> ${data.results[0].phone}</div>
+                </div>
+                <div class="tooltip">
+                    <button><img src="" alt="password"></button>
+                <div class="tooltiptext">You Hovered Me!</div>
+            </div>
+    </div>
     `;
 }
 
