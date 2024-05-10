9f39a21d656f57e5501d400498de380a8b5ca927
kenia <keniayhdiaz@gmail.com>
2024-05-10 07:45:58 -0500

    feat: :sparkles: NUevas funciones

--- Dia 16/js/script.js
+++ Dia 16/js/script.js
@@ -1,56 +1,48 @@
-function fetchUser() {
-    let xhr = new XMLHttpRequest();
-    let url = 'https://randomuser.me/api/';
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status === 200) {
-            let dato = JSON.parse(this.responseText);
-            displayUser(dato)
-        } else {
-            return ("Habemus error!");
-        }
-    }
-    xhr.send()
-}
-
-function displayUser(dato) {
-
-    
-    //Comando para obtener nombre
-    nombre = ((dato["results"][0]["name"]["first"]) + " " + (dato["results"][0]["name"]["last"]))
-    //Comando para obtener correo
-    correo = (dato["results"][0]["email"])
-
-    //Comando para obtener fecha de nacimiento
-    variable = dato["results"][0]["dob"]["date"];
-    variable = variable.slice(0, 10);
-    arrayFechas = variable.split('-');
-    ano = arrayFechas[0]
-    mes = arrayFechas[1]
-    dia = arrayFechas[2]
-    fechaFinal = mes + "/" + dia + "/" + ano
-    //Comando para obtener dirección
-    direccion = ((dato["results"][0]['location']['street']['number']) + " " + (dato["results"][0]['location']['street']['name']))
-    //Comando para obtener telefono
-    telefonoCompleto = dato["results"][0]['phone'];
-    arrayTel = telefonoCompleto.split(' ');
-    telefonoFinal = "(" + arrayTel[0] + ")" + " " + arrayTel[1] + "-" + arrayTel[2] + arrayTel[3];
-    //Comando para obtener password
-    contrasena = (dato["results"][0]["login"]["password"])
-    //Comando para la foto
-    foto = (dato["results"][0]['picture']['large'])
-    let buttom = document.getElementById('hola')
-    let userInfo = document.getElementById('userInfo');
-    if (dato.response == "Habemus error!") {
-        userInfo.innerHTML = `<h1>HABEMUS ERROR!!!</h1>`
-        buttom.innerHTML = `<h1>HABEMUS ERROR!!!</h1>`
-    } else {
-        userInfo.innerHTML = `
-                    
-            <img src='${foto}' id="foti"></img>
-
+class UserInfo extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = `
+            <div id="userInfo" class="userInfo"></div>
         `;
-        buttom.innerHTML = `           
+    }
+
+    connectedCallback() {
+        this.fetchUser();
+    }
+
+    fetchUser() {
+        let xhr = new XMLHttpRequest();
+        let url = 'https://randomuser.me/api/';
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status === 200) {
+                let dato = JSON.parse(xhr.responseText);
+                this.displayUser(dato);
+            } else {
+                this.displayError();
+            }
+        }
+        xhr.send();
+    }
+
+    displayUser(dato) {
+        const user = dato.results[0];
+        const nombre = `${user.name.first} ${user.name.last}`;
+        const correo = user.email;
+        const fecha = new Date(user.dob.date);
+        const fechaFinal = `${fecha.getMonth() + 1}/${fecha.getDate()}/${fecha.getFullYear()}`;
+        const direccion = `${user.location.street.number} ${user.location.street.name}`;
+        const telefonoCompleto = user.phone;
+        const arrayTel = telefonoCompleto.split(' ');
+        const telefonoFinal = `(${arrayTel[0]}) ${arrayTel[1]}-${arrayTel[2]}${arrayTel[3]}`;
+        const contrasena = user.login.password;
+        const foto = user.picture.large;
+        const buttom = this.getElementById('hola');
+        const userInfo = this.shadowRoot.getElementById('userInfo');
+
+        userInfo.innerHTML = `
+            <img src="${foto}" id="foti"></img>
             <ul class="values_list horizontal_center" id="values_list">
                 <li data-title="Hi, My name is" data-value="${nombre}" data-label="name" class="active" id="elemento"></li>
                 <li data-title="My email address is" data-value="${correo}" data-label="email" class=" " id="elemento"></li>
@@ -60,39 +52,41 @@ function displayUser(dato) {
                 <li data-title="My password is" data-value="${contrasena}" data-label="password" class=" " id="elemento"></li>
             </ul>
         `;
-        addEventListenerToLiElements();
+        this.addEventListenerToLiElements();
+    }
+
+    displayError() {
+        const userInfo = this.shadowRoot.getElementById('userInfo');
+        userInfo.innerHTML = `<h1>HABEMUS ERROR!!!</h1>`;
+        const buttom = document.getElementById('hola');
+        buttom.innerHTML = `<h1>HABEMUS ERROR!!!</h1>`;
+    }
+
+    addEventListenerToLiElements() {
+        const elementos = this.shadowRoot.querySelectorAll("#values_list li");
+        const infoDisplay = document.getElementById("info_Display");
+
+        elementos.forEach(elemento => {
+            if (elemento.classList.contains("active")) {
+                const dataTitle = elemento.getAttribute("data-title");
+                const dataValue = elemento.getAttribute("data-value");
+                infoDisplay.innerHTML = "<p>" + dataTitle + "</p>" + "<h3>" + dataValue + "</h3>";
+            }
+
+            elemento.addEventListener("mouseover", () => {
+                elementos.forEach(e => {
+                    e.classList.remove("active");
+                });
+                elemento.classList.add("active");
+                const dataTitle = elemento.getAttribute("data-title");
+                const dataValue = elemento.getAttribute("data-value");
+                infoDisplay.innerHTML = "<p>" + dataTitle + "</p>" + "<h3>" + dataValue + "</h3>";
+            });
+        });
     }
 }
 
-// Esta función agrega el event listener a los elementos <li>
-function addEventListenerToLiElements() {
-    var elementos = document.querySelectorAll("#values_list li");
-    var infoDisplay = document.getElementById("info_Display");
+customElements.define('user-info', UserInfo);
 
-    // Agrega un event listener a cada elemento
-    elementos.forEach(function(elemento) {
-        if (elemento.classList.contains("active")) { // Verifica si el elemento ya tiene la clase "active" al cargar la página
-            var dataTitle = elemento.getAttribute("data-title");
-            var dataValue = elemento.getAttribute("data-value");
-            infoDisplay.innerHTML = "<p>" + dataTitle + "</p>"  + "<h3>" + dataValue + "</h3>";
-        }
-
-        // Agrega un event listener para el evento "mouseover"
-        elemento.addEventListener("mouseover", function() {
-            elementos.forEach(function(e) { // Desactiva todos los elementos
-                e.classList.remove("active");
-            });
-            elemento.classList.add("active"); // Activa solo el elemento actual
-            var dataTitle = elemento.getAttribute("data-title");
-            var dataValue = elemento.getAttribute("data-value");
-            infoDisplay.innerHTML = "<p>" + dataTitle + "</p>"  + "<h3>" + dataValue + "</h3>";
-        });
-    });
-}
-
+// Iniciar la búsqueda del usuario al cargar la página
 fetchUser();
-
-
-//////////////////
-
-class 
